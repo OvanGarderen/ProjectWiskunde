6d8a773911ebdda08df64c26c908cd52c1e5122c
Jan Westerdiep <janner@gmail.com>
2013-05-14 17:08:08 +0200

    nu ook met extra tools extra

--- Tools.py
+++ Tools.py
@@ -1,4 +1,6 @@
 def find_cutoff_nd( array ):
   if ratio < 0 or ratio > 1: assert 0
-  output = np.reshape(array, reduce(lambda x, y: x * y, array.shape)) #reshape naar 1 lang ding
+  output = np.copy(array)
+  output = np.reshape(output, reduce(lambda x, y: x * y, output.shape)) #reshape naar 1 lang ding
+  output = np.sort( np.absolute( output ) )
   return output[(1-ratio)*len(output)]
