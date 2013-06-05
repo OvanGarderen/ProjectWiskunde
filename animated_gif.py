cd0c4d5efd1a9153c6bb53dd9c84eeb616e23f9a
OvanGarderen <ogiervangarderen@gmail.com>
2013-06-05 18:20:28 +0200

    CLI voor animated

--- animated_gif.py
+++ animated_gif.py
@@ -9,10 +9,14 @@ from Wavelet_Defs import wavelet_dict
 from driedeeding import mat_3d_to_dict, dict_to_mat_3d
 from wave_img import mat5img
 
-wave = wavelet_dict['haar']
+
 
 def plaatje(plaat):
-  mytensor = False
+  global mywavelet, mycompress, myoutfile, mytensor, mydelay, mytensor
+
+  print "compressing",plaat
+  print "tensor is %s" % ("on" if mytensor else "off")
+  print 
   print "splitting up gif... (this may take a while)"
   call(["gifsicle", "--unoptimize", "-e", plaat, "-o", plaat])
 
@@ -28,11 +32,11 @@ def plaatje(plaat):
   print np.array(data).shape
   data_float = map( lambda x: x.astype('float'), data )
 
-  encoded = np.array(map( lambda x: wave.dwt(x,tensor=mytensor), data_float ))
+  encoded = np.array(map( lambda x: mywavelet.dwt(x,tensor=mytensor), data_float ))
   interdims = encoded[0].shape
   print "interdims:", interdims
 
-  compression = find_cutoff_nd( encoded, 0.01 )
+  compression = find_cutoff_nd( encoded, mycompress )
 
   dicks = map( lambda x: mat_3d_to_dict( x, compression ), encoded )
   print map( lambda x: compress( x[0], x[1] ), zip( dicks, encoded ) )
@@ -40,7 +44,7 @@ def plaatje(plaat):
   lists = map( lambda x: dict_to_mat_3d( x, interdims[0], interdims[1], interdims[2]), dicks )
   lists = map( lambda x: np.array(x), lists )
 
-  decoded = map( lambda x: wave.idwt(x,tensor=mytensor), lists )
+  decoded = map( lambda x: mywavelet.idwt(x,tensor=mytensor), lists )
   sliced = map( lambda x: np.rint(x).astype( data[0].dtype ), decoded )
   print PSNR( np.array(data), np.array(sliced) )
 
@@ -61,12 +65,83 @@ def plaatje(plaat):
     mat5img(frame, (rows, cols) ).save( fn )
     os.system(' '.join(["convert", fn, fn.replace("png", "gif")]))
 
-  print "creating gif"
-  os.system(''.join(["gifsicle -d 10 --loopcount=forever -m ","gif/temp.*.gif"," > ",plaat.replace(".", "_new.")]))
+  if myoutfile == None:
+    filename = plaat.replace('.','_new.')
+  else:
+    filename = myoutfile
+  print "Saving gif to",filename
+  os.system(''.join(
+    ["gifsicle -d ",
+     str(mydelay),
+     " --loopcount=forever -m ",
+     "gif/temp.*.gif"
+     ," > ",filename]))
 
   print "removing temp files"
   call(["rm"]+list(glob('gif/temp.*.gif')))
   call(["rm"]+list(glob('gif/temp.*.png')))
 
 if __name__ == "__main__":
-  plaatje("gif/boule.gif")
+  # Dit stuk regelt de CLI options
+  from sys import stderr
+  import CLI,sys
+  my_opts = [
+    # short, long, args, description
+    ('t','tensor',0,"zet tensor aan"),
+    ('w','wavelet',1,"select wavelet <arg>"),
+    ('c','compress',1,"set compression to <arg>"),
+    ('o','output',1,"set destination to <arg>"),
+    ('d','delay',1,"set frame-delay to <arg>"),
+    ('h','help',0,'print this message')
+  ]
+  my_usage = CLI.usage("python animated_gif.py [gif] [options]",
+                      "Uses various wavelets to compress and then decompress an animated gif",
+                       my_opts)
+  try:
+    args, opts = CLI.parse_args(sys.argv,my_opts) #throws IOError on error
+  except IOError:
+    print my_usage.short()
+    exit(1)
+
+  #set defaults for various options
+  from Wavelet_Defs import wavelet_dict
+  mywavelet = wavelet_dict['haar']
+  mycompress = 1.0
+  myoutfile = None
+  mytensor = False
+  mydelay = 10
+
+  for o in opts:
+    if o[0] == 'wavelet':
+      try:
+        mywavelet = wavelet_dict[o[1]]
+      except:
+        print >>stderr,o[1],'is not a valid wavelet'
+        exit(1)
+    elif o[0] == 'compress':
+      try:
+        mycompress = float(o[1]) 
+      except TypeError:
+        exit(1)
+    elif o[0] == 'output':
+      myoutfile = o[1]
+        
+    elif o[0] == 'tensor':
+      mytensor = True
+
+    elif o[0] == 'help':
+      print my_usage
+      exit(1)
+    
+    elif o[0] == 'delay':
+      try:
+        mydelay = int(o[1])
+      except TypeError:
+        exit(1)
+
+  if len(args) == 1: # additional IOError
+    print my_usage.short()
+    exit(1)
+  
+  map(plaatje,args[1:])
+
