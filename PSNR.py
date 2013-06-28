d853c23f0c639ddf8cc16ebcd11bb23ddeaa6487
Jan Westerdiep <janner@gmail.com>
2013-06-28 11:46:38 +0200

    psnr kan je nu aanroepen

--- PSNR.py
+++ PSNR.py
@@ -1,5 +1,7 @@
 #Peak signal to noise ratio implementatie. zie wikipedia
 import numpy as np
+from channels import *
+from sys import argv
 from math import log
 
 def PSNR( s1, s2 ):
@@ -10,3 +12,9 @@ def PSNR( s1, s2 ):
   if sum_squared == 0: return np.inf
   psnr = 20 * log(maximal_value, 10) - 10*log(sum_squared, 10)
   return psnr
+
+if __name__ == "__main__":
+  if len(argv) > 1:
+    img1 = np.array(img3mat(argv[1])[0])
+    img2 = np.array(img3mat(argv[2])[0])
+    print PSNR( img1, img2 )
